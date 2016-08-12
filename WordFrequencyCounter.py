feb55696d63b3a836be141f278557db01ae04cb4
keilabs <kei.developers@gmail.com>
2016-08-12 20:43:59 +0800

    Added zips.py

--- WordFrequencyCounter.py
+++ WordFrequencyCounter.py
@@ -5,7 +5,7 @@ import operator
 def start(url):
     word_list = []
     source_code = requests.get(url).text
-    soup = BeautifulSoup(source_code, html.parser)
+    soup = BeautifulSoup(source_code)
     for post_text in soup.findAll('a', {'class': 'color_0077'}):
         content = post_text.string
         words = content.lower().split()
